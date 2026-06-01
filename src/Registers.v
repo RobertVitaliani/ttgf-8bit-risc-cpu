240ecb08282f6f12d76896e678163e6494cbdb10
Robert Vitaliani <robert.vitaliani@gmail.com>
2026-06-01 20:30:32 +0200

    Expand small memories to explicit registers

--- src/Registers.v
+++ src/Registers.v
@@ -2,34 +2,60 @@
 module Registers (
     input clk, input rst, input RegWrite,input [2:0] rs1, input [2:0] rs2, input [2:0] rd, input [7:0] write_data, output [7:0] reg_data_1, output [7:0] reg_data_2);
     
-    reg[7:0] register_file[7:0];
-    
+    reg [7:0] register_0;
+    reg [7:0] register_1;
+    reg [7:0] register_2;
+    reg [7:0] register_3;
+    reg [7:0] register_4;
+    reg [7:0] register_5;
+    reg [7:0] register_6;
+    reg [7:0] register_7;
 
     always @(posedge rst or posedge clk)
     begin
         if(rst) begin
-            /*
-            for(k=0;k<31;k=k+1) begin
-                Registers[k]<=32'b0;
-            end*/
-            register_file[0]<=1;
-            register_file[1]<=4;
-            register_file[2]<=2;
-            register_file[3]<=24;
-            register_file[4]<=0;
-            register_file[5]<=4;
-            register_file[6]<=2;
-            register_file[7]<=24;
+            register_0<=1;
+            register_1<=4;
+            register_2<=2;
+            register_3<=24;
+            register_4<=0;
+            register_5<=4;
+            register_6<=2;
+            register_7<=24;
             
         end
 
         else if(RegWrite) begin
-            register_file[rd]<= write_data;
+            case (rd)
+                3'd0: register_0<= write_data;
+                3'd1: register_1<= write_data;
+                3'd2: register_2<= write_data;
+                3'd3: register_3<= write_data;
+                3'd4: register_4<= write_data;
+                3'd5: register_5<= write_data;
+                3'd6: register_6<= write_data;
+                default: register_7<= write_data;
+            endcase
         end
     end
 
-    assign reg_data_1=register_file[rs1];
-    assign reg_data_2=register_file[rs2];
+    assign reg_data_1 = (rs1 == 3'd0) ? register_0 :
+                        (rs1 == 3'd1) ? register_1 :
+                        (rs1 == 3'd2) ? register_2 :
+                        (rs1 == 3'd3) ? register_3 :
+                        (rs1 == 3'd4) ? register_4 :
+                        (rs1 == 3'd5) ? register_5 :
+                        (rs1 == 3'd6) ? register_6 :
+                                        register_7;
+
+    assign reg_data_2 = (rs2 == 3'd0) ? register_0 :
+                        (rs2 == 3'd1) ? register_1 :
+                        (rs2 == 3'd2) ? register_2 :
+                        (rs2 == 3'd3) ? register_3 :
+                        (rs2 == 3'd4) ? register_4 :
+                        (rs2 == 3'd5) ? register_5 :
+                        (rs2 == 3'd6) ? register_6 :
+                                        register_7;
     //forwarding ako se u t2 koristi rezultat iz t1
     //assign readData1 = (readReg1 == writeReg && regWrite) ? writeData : registers[readReg1];
 endmodule
